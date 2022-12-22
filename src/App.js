58ac568d847d50616e03ac2dce8e283940ab53c9
Mykhailo Pomorskyi <mishapomorskyi@gmail.com>
2022-12-22 15:02:40 +0100

    Fixing deploying router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import {
-  Routes,
-  Route
-} from "react-router-dom";
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 import { ThemeContextProvider } from './Contextes/ThemeContext'
-import { Container } from 'react-bootstrap';
-import SortingPage from './Pages/SortingPage';
-import { CookiesProvider } from 'react-cookie';
-import Footer from './Components/Footer';
+import { Container } from 'react-bootstrap'
+import SortingPage from './Pages/SortingPage'
+import { CookiesProvider } from 'react-cookie'
+import Footer from './Components/Footer'
 
 function App() {
-
   return (
     <CookiesProvider>
       <ThemeContextProvider>
         <Routes>
-          <Route path='/' element={<SortingPage />} />
-          <Route path='/:sortingType' element={<SortingPage />} />
+          <Route path='/' exact element={<SortingPage />} />
+          <Route path='/:sortingType' exact element={<SortingPage />} />
         </Routes>
         <Container fluid>
           <Footer />
         </Container>
       </ThemeContextProvider>
     </CookiesProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter as Router } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Router>
-    <App className='app'/>
-  </Router>
-);
-
+  <React.StrictMode>
+    <Router>
+      <App className='app' />
+    </Router>
+  </React.StrictMode>
+)
