74a0bf5491749088f3d15bdbc58b136a71972f33
Mykhailo Pomorskyi <mykhailo.pomorskyi@sollers.eu>
2023-01-11 14:26:15 +0100

    Deploy

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
-import { ThemeContext } from '../Contextes/ThemeContext';
-import useBoard from './sorting/useBoardHook';
-import './NavigationBar.css'
+import React, { useContext } from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { ThemeContext } from "../Contextes/ThemeContext";
+import useBoard from "./sorting/useBoardHook";
+import "./NavigationBar.css";
 
 const NavigationBar = () => {
   const { navTitle } = useBoard();
-  const { onChangeTheme, darkActive } = useContext(ThemeContext)
+  const { onChangeTheme, darkActive } = useContext(ThemeContext);
 
   return (
     <Navbar expand="lg" className="navbar_theming">
@@ -15,47 +15,51 @@ const NavigationBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title={navTitle} id="basic-nav-dropdown">
-              <NavDropdown.Item href="/selection-sort">
+              <NavDropdown.Item href="/sorting-algorithms/selection-sort">
                 Selection sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/bubble-sort">
+              <NavDropdown.Item href="/sorting-algorithms/bubble-sort">
                 Bubble sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/insertion-sort">
+              <NavDropdown.Item href="/sorting-algorithms/insertion-sort">
                 Insertion sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/merge-sort">
+              <NavDropdown.Item href="/sorting-algorithms/merge-sort">
                 Merge sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/quick-sort">
+              <NavDropdown.Item href="/sorting-algorithms/quick-sort">
                 Quick sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/heap-sort">
+              <NavDropdown.Item href="/sorting-algorithms/heap-sort">
                 Heap sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/counting-sort">
+              <NavDropdown.Item href="/sorting-algorithms/counting-sort">
                 Counting sort
               </NavDropdown.Item>
-              <NavDropdown.Item href="/radix-sort">
+              <NavDropdown.Item href="/sorting-algorithms/radix-sort">
                 Radix sort
               </NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/">
+              <NavDropdown.Item href="/sorting-algorithms/">
                 About sorting
               </NavDropdown.Item>
             </NavDropdown>
-
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className='theming_change_btn'>
-            <input type="checkbox" onChange={onChangeTheme} defaultChecked={darkActive}/> isDark
+          <Navbar.Text className="theming_change_btn">
+            <input
+              type="checkbox"
+              onChange={onChangeTheme}
+              defaultChecked={darkActive}
+            />{" "}
+            isDark
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
